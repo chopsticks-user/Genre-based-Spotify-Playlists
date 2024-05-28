6e45274569cabbc0858d50a6bc87ff2bc6aaebd7
asanjideh <ariana.sanjideh@student.unimelb.edu.au>
2024-05-28 13:41:36 -0400

    Added loading screen and put app logo on welcome screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,70 @@
 import { useEffect, useState } from 'react'
-import { View, Text, SafeAreaView, Button, Image, StyleSheet, Platform } from 'react-native'
+import { View, Text, SafeAreaView, Button, Image, StyleSheet, TouchableOpacity, Platform } from 'react-native'
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedView } from '@/components/ThemedView';
+import * as firebase from 'firebase'
 import * as AuthSession from 'expo-auth-session';
-import { response } from 'express';
 import * as Configs from '@/configs'
+import SplashScreen from '@/components/SplashScreen';
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDR-k84V8VE1W6zLtDam4awkoxtsZXYf3c",
+  authDomain: "playtify-8bf88.firebaseapp.com",
+  projectId: "playtify-8bf88",
+  storageBucket: "playtify-8bf88.appspot.com",
+  messagingSenderId: "178512786596",
+  appId: "1:178512786596:web:b2a46a68aeca2f13542231"
+};
+
+const app = initializeApp(firebaseConfig);
 
 const styles = StyleSheet.create({
     titleContainer: {
         flexDirection: 'row',
         alignItems: 'center',
-        gap: 8,
+        gap: 6,
     },
     stepContainer: {
         gap: 8,
         marginBottom: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
     },
     reactLogo: {
-        height: 250,
-        width: 400,
+        height: 300,
+        width: 370,
         bottom: 0,
         left: 0,
         position: 'absolute',
+    },
+     connectButton: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 15,
+        backgroundColor: '#fff',
+        borderRadius: 25,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        width: 300,
+    },
+    buttonIcon: {
+        width: 35,
+        height: 35,
+        marginRight: 10,
+    },
+    buttonText: {
+        fontSize: 16,
+        fontWeight: 'bold',
     },
 });
 
@@ -36,6 +79,12 @@ async function getAuthCode() {
 }
 
 export default function Home() {
+    const [isLoading, setIsLoading] = useState(true);
+
+    const handleLoadingComplete = () => {
+        setIsLoading(false);
+    };
+
     try {
         const discovery: AuthSession.DiscoveryDocument = {
             authorizationEndpoint: "https://accounts.spotify.com/authorize",
@@ -64,22 +113,25 @@ export default function Home() {
         console.log("Error:" + err);
     }
 
+    if (isLoading) {
+        return <SplashScreen onLoadingComplete={handleLoadingComplete} />;
+    }
+
     return (
         <ParallaxScrollView
             headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
             headerImage={
                 <Image
-                    source={require('@/assets/images/spotify-logo.jpg')}
+                    source={require('@/assets/images/applogo.jpg')}
                     style={styles.reactLogo}
                 />
             }>
             <ThemedView style={styles.stepContainer}>
-                <Button title="Connect to Spotify" onPress={async (event) => {
-                    console.log("Connect to Spotify");
-                    prompt();
-                }
-                } />
-            </ThemedView>
-        </ParallaxScrollView>
+                    <TouchableOpacity onPress={() => prompt()} style={styles.connectButton}>
+                        <Image source={require('@/assets/images/spotifylogosolo.jpg')} style={styles.buttonIcon} />
+                        <Text style={styles.buttonText}>Sign in with Spotify</Text>
+                    </TouchableOpacity>
+                </ThemedView>
+            </ParallaxScrollView>
     );
 }
