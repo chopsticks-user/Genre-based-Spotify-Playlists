3e3a3d5e9d787d2adb654e509613d4668d5b043a
chopsticks-user <frostyfrost273@gmail.com>
2024-05-28 20:37:41 -0400

    Fixed not showing back buttons in ios

--- app/settings/_layout.tsx
+++ app/settings/_layout.tsx
@@ -1,17 +1,35 @@
-import { Stack } from "expo-router";
+import { Stack, router } from "expo-router";
+import { Platform } from "react-native";
+import { HeaderBackButton } from '@react-navigation/elements';
+
+const screenOptions: any = {
+    headerShown: true,
+    headerStyle: {
+        backgroundColor: '#000',
+    },
+    headerTitleAlign: 'center',
+    headerTintColor: '#fff',
+    headerBackButtonMenuEnabled: true,
+    // tabBarStyle: {
+    //     backgroundColor: '#000',
+    // },
+};
 
 export default function SettingsLayout() {
+    if (Platform.OS === 'ios') {
+        screenOptions.headerLeft = (props: any) => (
+            <HeaderBackButton
+                onPress={() => {
+                    router.back();
+                }}
+            />
+        );
+        return (
+            <Stack screenOptions={screenOptions} />
+        );
+    }
+
     return (
-        <Stack screenOptions={{
-            headerShown: true,
-            headerStyle: {
-                backgroundColor: '#000',
-            },
-            headerTitleAlign: 'center',
-            headerTintColor: '#fff',
-            // tabBarStyle: {
-            //     backgroundColor: '#000',
-            // },
-        }} />
+        <Stack screenOptions={screenOptions} />
     );
 }
